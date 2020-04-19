08d62d9c9d5298a53c7caeb2480f39ca36fccceb
Ronald Neftali Berdúo Morales <ronaldnef1996@gmail.com>
2020-04-19 01:31:19 -0600

    Project init

---
+++ src/utils/logger.ts
@@ -0,0 +1,54 @@
+import { createLogger, format, transports } from "winston";
+import path from "path";
+
+const consoleFile = format.combine(
+  format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
+  format.printf((info) => {
+    const { timestamp, level, message } = info;
+    return `[${timestamp}] ${level}:  ${message}`;
+  })
+);
+
+const consoleFormat = format.combine(format.colorize(), consoleFile);
+
+function tranportConsole() {
+  return new transports.Console({
+    level: "debug",
+    handleExceptions: true,
+    format: consoleFormat,
+  });
+}
+
+function transportFile(
+  filename: string,
+  level: string,
+  maxsize: number,
+  maxFiles: number
+) {
+  return new transports.File({
+    filename,
+    level,
+    maxsize,
+    maxFiles,
+    format: consoleFile,
+  });
+}
+
+export const logger = createLogger({
+  transports: [
+    tranportConsole(),
+    transportFile(
+      path.join(__dirname, "../logs/error.log"),
+      "error",
+      5242880,
+      5
+    ),
+    transportFile(path.join(__dirname, "../logs/info.log"), "info", 5242880, 5),
+  ],
+  exitOnError: false,
+});
+
+export const stream = {
+  write: (message: string) =>
+    logger.info(message.substring(0, message.lastIndexOf("\n"))),
+};
